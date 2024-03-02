338cb120d50fd13ebefdddaa29ce8712817cdd74
Matthew Rey <matthewrey@zayls.com>
2024-03-02 12:17:17 +0800

    Add blogRouter

--- index.js
+++ index.js
@@ -3,26 +3,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blog");
 
 mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
